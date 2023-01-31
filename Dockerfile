1ed65256b8c6dfd53da1225edf4e952cbef414cc
piwi <you@example.com>
2023-01-31 20:39:00 +1000

    added testing celery task

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM python:3.9-alpine3.16
 
+RUN apk add postgresql-client build-base postgresql-dev
 COPY requirements.txt /temp/requirements.txt
 RUN pip install -r /temp/requirements.txt
 COPY service /service
 WORKDIR /service
 EXPOSE 8000
-
-RUN apk add postgresql-client build-base postgresql-dev
 
 RUN adduser --disabled-password piwi
 
--- service/service/settings.py
+++ service/service/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure--^!5(ew@jahbp0j0b#@6(9s4b5z_f22mw(-*y7!#par%1@g0rn
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "service.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +111,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -132,3 +127,4 @@ LOGGING = {
     "handlers": {"console": {"class": "logging.StreamHandler"}},
     "loggers": {"django.db.backends": {"handlers": ["console"], "level": "DEBUG"}},
 }
+CELERY_BROKER_URL = 'redis://redis:6379/0'
